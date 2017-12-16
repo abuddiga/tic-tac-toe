405472e4d2a0a3c416c231f79e9f4a10d621e969
Akshay Buddiga <abuddiga@fanatics.com>
2017-12-16 15:20:36 -0800

    Delete old code

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,44 +1,5 @@
 pragma solidity 0.4.18;
 
-
-// contract TicTacToe {
-
-//     // NOTE: All of the content here is just to provide some test examples
-
-//     address owner;
-
-//     // Storage variable must be public if we want automatically have a getter for it
-//     uint256 public state;
-
-//     event StateChanged(uint256 changedTo);
-//     event Error(string msg);
-
-//     function TicTacToe() {
-//         owner = msg.sender;
-//     }
-
-//     // Function to test return value, state changes and event emission
-//     function main(uint256 _newState) public returns(bool) {
-//         if (msg.sender != owner) {
-//             Error("You are not the owner");
-//             return false;
-//         }
-//         state = _newState;
-//         StateChanged(_newState);
-//         return true;
-//     }
-
-//     // Function to test VM exception
-//     function other(uint256 _newState) public returns(bool) {
-//         state = _newState;
-//         StateChanged(_newState);
-//         // Require goes after state changes in order to demonstrate changes reversion
-//         require(msg.sender != owner);
-//         return true;
-//     }
-
-// }
-
 contract TicTacToe {
     address public playerOne;
     address public playerTwo;
@@ -129,7 +90,7 @@ contract TicTacToe {
             winner = currentTurn;
             ended = true;
             GameWon(winner);
-        } else if (moves == 8) {
+        } else if (moves == 9) {
             GameDrawn("The game is a draw!");
             ended = true;
         }
