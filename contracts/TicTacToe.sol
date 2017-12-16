2b149ca1005489ee63c25b11ce3a33d306acd152
Akshay Buddiga <abuddiga@fanatics.com>
2017-12-16 14:25:53 -0800

    Using structs

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,75 +1,161 @@
 pragma solidity 0.4.18;
 
 
+// contract TicTacToe {
+
+//     // NOTE: All of the content here is just to provide some test examples
+
+//     address owner;
+
+//     // Storage variable must be public if we want automatically have a getter for it
+//     uint256 public state;
+
+//     event StateChanged(uint256 changedTo);
+//     event Error(string msg);
+
+//     function TicTacToe() {
+//         owner = msg.sender;
+//     }
+
+//     // Function to test return value, state changes and event emission
+//     function main(uint256 _newState) public returns(bool) {
+//         if (msg.sender != owner) {
+//             Error("You are not the owner");
+//             return false;
+//         }
+//         state = _newState;
+//         StateChanged(_newState);
+//         return true;
+//     }
+
+//     // Function to test VM exception
+//     function other(uint256 _newState) public returns(bool) {
+//         state = _newState;
+//         StateChanged(_newState);
+//         // Require goes after state changes in order to demonstrate changes reversion
+//         require(msg.sender != owner);
+//         return true;
+//     }
+
+// }
+
 contract TicTacToe {
 
-    // NOTE: All of the content here is just to provide some test examples
-
-    address owner;
-
-    // Storage variable must be public if we want automatically have a getter for it
-    uint256 public state;
-
-    event StateChanged(uint256 changedTo);
-    event Error(string msg);
-
-    function TicTacToe() {
-        owner = msg.sender;
+    struct Player {
+        address playerId;
+        uint score;
     }
 
-    // Function to test return value, state changes and event emission
-    function main(uint256 _newState) public returns(bool) {
-        if (msg.sender != owner) {
-            Error("You are not the owner");
-            return false;
-        }
-        state = _newState;
-        StateChanged(_newState);
-        return true;
-    }
-
-    // Function to test VM exception
-    function other(uint256 _newState) public returns(bool) {
-        state = _newState;
-        StateChanged(_newState);
-        // Require goes after state changes in order to demonstrate changes reversion
-        require(msg.sender != owner);
-        return true;
-    }
-
-}
-
-contract TicTacToe {
-    address public playerOne;
-    address public playerTwo;
-    address public currentTurn;
-    address public winner;
+    Player public playerOne;
+    Player public playerTwo;
+    Player public currentTurn;
+    Player public nextPlayer;
+    Player public winner;
 
     bool public ended;
 
-    enum SquareStates {OwnedByPlayerOne, OwnedByPlayerTwo}
+    // uint playerOneScore;
+    // uint playerTwoScore;
 
-    SquareStates[3][3] public board;
+    uint[9] squareValues = [1, 2, 4, 8, 16, 32, 64, 128, 256];
+    uint[8] winningScores = [7, 56, 73, 84, 146, 273, 292, 448];
+    uint[9] occupied; // in the occupied array, 1s represent occupied squares (by either player)
 
-    event GameEnded(address winner);
+    event GameEnded(Player winner);
+    event Error(string msg);
 
+    modifier gameNotOver() {
+        require(!ended);
+        _;
+    }
+
+    modifier isPlayersTurn(address sender) {
+        require(sender == currentTurn.playerId);
+        _;
+    }
+
+    modifier squareIsEmpty(uint sq) {
+        require(sq >= 0 && sq < 9);
+        require(occupied[sq] == 0);
+        _;
+    }
 
     /// Create a new Tic-Tac-Toe Game
     function TicTacToe() public {
-        playerOne = msg.sender;
-        currentTurn = msg.sender;
+        playerOne = Player({
+            playerId: msg.sender,
+            score: 0
+        });
+        currentTurn = playerOne;
         ended = false;
     }
 
     /// Join game as playerTwo
-    function joinGame() public {
-        require(playerOne != 0);
-        require(!ended);
-        playerTwo = msg.sender;
+    function joinGame()
+    public
+    gameNotOver
+    returns(bool) {
+        require(playerOne.playerId != 0);
+        require(playerTwo.playerId == 0);
+        // gameNotOver;
+        playerTwo = Player({
+            playerId: msg.sender,
+            score: 0
+        });
         return true;
     }
 
-    function makeMove() public returns(bool) {
+    /// Enter the int (0 - 8) of the square you would like to move to.
+    /// The top left square is 0, the square to its right is 1, and so on.
+    /// The bottom right square is 8.
+    function makeMove(uint square)
+    public
+    gameNotOver
+    isPlayersTurn(msg.sender)
+    squareIsEmpty(square)
+    returns(bool) {
+        uint scoreToCheck;
 
+        // conditions
+        // gameNotOver;
+        // isPlayersTurn(msg.sender);
+        // squareIsEmpty(square);
+
+        // effects
+
+        if (msg.sender == playerOne.playerId) {
+            playerOne.score += squareValues[square];
+            scoreToCheck = playerOne.score;
+            nextPlayer = playerTwo;
+
+        } else if (msg.sender == playerTwo.playerId) {
+            playerTwo.score += squareValues[square];
+            scoreToCheck = playerTwo.score;
+            nextPlayer = playerOne;
+        } else {
+            Error("You are not one of the two players of this game!");
+            return false;
+        }
+
+        occupied[square] = 1;
+
+        if (checkForWin(scoreToCheck)) {
+            winner = currentTurn;
+            ended = true;
+            GameEnded(winner);
+        }
+
+        currentTurn = nextPlayer;
+
+        return true;
+    }
+
+    function checkForWin(uint score) private returns(bool) {
+        for (uint i = 0; i < winningScores.length; i += 1) {
+            if ((winningScores[i] & score) == winningScores[i]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
